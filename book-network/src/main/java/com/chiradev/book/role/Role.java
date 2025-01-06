9526f18d3e38b14e1014f080d2a786eb627468a1
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-01-06 23:14:58 +0530

    Add User, Role, and Token entities along with their repositories
    This captures the essence of the changes, indicating the creation of three entities and their respective repositories

---
+++ book-network/src/main/java/com/chiradev/book/role/Role.java
@@ -0,0 +1,40 @@
+package com.chiradev.book.role;
+
+import com.chiradev.book.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDate createdDate;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private LocalDate lastModifiedDate;
+}
