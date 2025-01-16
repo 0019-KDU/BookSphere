f27b4acacf147a6f16d0927d7b0df1c8769b3634
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-01-16 22:02:38 +0530

    add Book, Feedback, and BookTransactionHistory entities with inheritance and relationships

---
+++ book-network/src/main/java/com/chiradev/book/book/Book.java
@@ -0,0 +1,53 @@
+package com.chiradev.book.book;
+
+import com.chiradev.book.common.BaseEntity;
+import com.chiradev.book.feedback.Feedback;
+import com.chiradev.book.history.BookTransactionHistory;
+import com.chiradev.book.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+
+    private String title;
+
+    private String authorName;
+
+    private String isbn;
+
+    private String synopsis;
+
+    private String bookCover;
+
+    private boolean archived;
+
+    private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name="owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+}
--- book-network/src/main/java/com/chiradev/book/user/User.java
+++ book-network/src/main/java/com/chiradev/book/user/User.java
@@ -1,5 +1,7 @@
 package com.chiradev.book.user;
 
+import com.chiradev.book.book.Book;
+import com.chiradev.book.history.BookTransactionHistory;
 import com.chiradev.book.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
@@ -41,6 +43,12 @@ public class User implements UserDetails, Principal {
 
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> histories;
 
     @CreatedDate
     @Column(nullable = false,updatable = false)
