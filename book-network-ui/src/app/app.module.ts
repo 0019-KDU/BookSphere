6ba508216a48a1857e240f9633995f534ce70e70
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-01-29 01:41:14 +0530

    Implemented Login, Registration, and Activate Account pages and CORS bug fix

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './pages/register/register.component';
+
+import { CodeInputModule } from 'angular-code-input';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ActivateAccountComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CodeInputModule],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    HttpClient,
+    provideHttpClient(),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { AuthenticationRequest } from '../../services/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { TokenService } from '../../services/token/token.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  authRequest: AuthenticationRequest = { email: '', password: '' };
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private tokenService: TokenService
+  ) {}
+
+  register() {
+    this.router.navigate(['register']);
+  }
+  login() {
+    this.errorMsg = [];
+    this.authService
+      .authenticate({
+        body: this.authRequest,
+      })
+      .subscribe({
+        next: (res) => {
+          this.tokenService.token = res.token as string;
+          this.router.navigate(['books']);
+        },
+        error: (err) => {
+          console.log(err);
+          if (err.error.validationErrors) {
+            this.errorMsg = err.error.validationErrors;
+          } else {
+            this.errorMsg.push(err.error.errorMsg);
+          }
+        },
+      });
+  }
+}
